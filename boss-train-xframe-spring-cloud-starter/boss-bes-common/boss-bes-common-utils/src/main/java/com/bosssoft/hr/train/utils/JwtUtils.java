b6ae5a97171ab6279a04b7553f8e5c2ad2014f19
dev01 <dev01@email.com>
2019-08-15 17:25:44 +0800

    完善工具类

---
+++ boss-train-xframe-spring-cloud-starter/boss-bes-common/boss-bes-common-utils/src/main/java/com/bosssoft/hr/train/utils/JwtUtils.java
@@ -0,0 +1,53 @@
+package com.bosssoft.hr.train.utils;
+
+import com.google.gson.Gson;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Map;
+
+/**
+ * 用于与jwt操作有关的接口
+ *
+ * @author : wukeqiang
+ * @version : 1.0.0
+ * @date : 2019/8/15
+ */
+public class JwtUtils {
+    /**
+     * 解密
+     * @param jsonWebToken '解析json'
+     * @param base64Security '设置解密方法'
+     * @return '解密后的json数据'
+     */
+    public static Claims parseJwt(String jsonWebToken, String base64Security) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(base64Security.getBytes())
+                    .parseClaimsJws(jsonWebToken).getBody();
+            return claims;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 前三个参数为自己用户token的一些信息比如id，权限，名称等。不要将隐私信息放入（大家都可以获取到）
+     * @param map '将要存入token的信息设置为map'
+     * @param base64Security '声明加密方法'
+     * @return ‘生成的token’
+     */
+    public static String createJwt(Map<String, Object> map, String base64Security) {
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        //添加构成JWT的参数
+        JwtBuilder builder = Jwts.builder().setHeaderParam("typ", "JWT")
+                .setPayload(new Gson().toJson(map))
+                .signWith(signatureAlgorithm,base64Security.getBytes());
+        //生成JWT
+        return builder.compact();
+    }
+
+}
---
+++ boss-train-xframe-spring-cloud-starter/boss-bes-common/boss-bes-common-utils/src/test/java/com/bosssoft/hr/train/utils/StringUtilsTest.java
@@ -0,0 +1,8 @@
+package com.bosssoft.hr.train.utils;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+public class StringUtilsTest {
+
+}
