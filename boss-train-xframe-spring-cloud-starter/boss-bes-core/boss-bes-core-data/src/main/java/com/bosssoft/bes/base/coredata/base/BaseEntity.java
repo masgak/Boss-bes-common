344f99ecf63ea0e6410715909d5350e1a0a2c148
wolfishness <961053359@qq.com>
2019-08-26 10:58:26 +0800

    添加base信息(BaseVO,BaseDTO,BaseEntity)

---
+++ boss-train-xframe-spring-cloud-starter/boss-bes-core/boss-bes-core-data/src/main/java/com/bosssoft/bes/base/coredata/base/BaseEntity.java
@@ -0,0 +1,11 @@
+package com.bosssoft.bes.base.coredata.base;
+
+/**
+ * @ClassName: BaseEntity
+ * @Description: TODO
+ * @Author: lujinshan
+ * @Date: 2019/8/26 10:17
+ * @Version: 1.0.0
+ */
+public class BaseEntity {
+}
---
+++ boss-train-xframe-spring-cloud-starter/boss-bes-core/boss-bes-core-data/src/main/java/com/bosssoft/bes/base/coredata/base/BaseVO.java
@@ -0,0 +1,11 @@
+package com.bosssoft.bes.base.coredata.base;
+
+/**
+ * @ClassName: BaseVO
+ * @Description: TODO
+ * @Author: lujinshan
+ * @Date: 2019/8/26 10:17
+ * @Version: 1.0.0
+ */
+public class BaseVO {
+}
