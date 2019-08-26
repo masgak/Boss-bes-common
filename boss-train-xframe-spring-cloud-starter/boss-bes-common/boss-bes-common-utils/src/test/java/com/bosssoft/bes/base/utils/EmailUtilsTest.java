33105154b55125580a4c4b238ff860f9db270673
wolfishness <961053359@qq.com>
2019-08-26 10:56:44 +0800

    修改EmailUtils，实现自动配置，通过配置信息读入相关数据，通过注解使用发送邮件

--- boss-train-xframe-spring-cloud-starter/boss-bes-common/boss-bes-common-utils/src/test/java/com/bosssoft/bes/base/utils/EmailUtilsTest.java
+++ boss-train-xframe-spring-cloud-starter/boss-bes-common/boss-bes-common-utils/src/test/java/com/bosssoft/bes/base/utils/EmailUtilsTest.java
@@ -2,13 +2,18 @@ package com.bosssoft.bes.base.utils;
 
 import org.junit.Test;
 
+import javax.annotation.Resource;
+
 public class EmailUtilsTest {
+
+    @Resource
+    EmailUtils emailUtils;
     @Test
     public void sendEmailTest(){
         String subject = "快递通知";
         String msg = "很抱歉，您的包裹在泉州地区被台风刮走";
         try {
-            EmailUtils.sendSimpleMail("2609339303@qq.com,18860127149@163.com",subject,msg);
+            emailUtils.sendSimpleMail("2609339303@qq.com,18860127149@163.com",subject,msg);
         }catch (Exception e){
             e.printStackTrace();
         }
