6188007b4832085d36c4304f449a0ee9372db192
李旭阳 <1026226770@qq.com>
2019-09-12 09:08:24 +0800

    增加试卷服务异常

--- boss-train-xframe-spring-cloud-starter/boss-bes-common-exception-logging/src/main/java/com/bosssoft/bes/base/enums/PaperExceptionEnum.java
+++ boss-train-xframe-spring-cloud-starter/boss-bes-common-exception-logging/src/main/java/com/bosssoft/bes/base/enums/PaperExceptionEnum.java
@@ -6,6 +6,11 @@ public enum PaperExceptionEnum {
      */
     PAPER_SEARCH_ERROR(230101, "查询结果异常"),
     PAPER_QUICK_COMB_EXAM_NON_EXISTENT(230102, "快速组卷的时候选择的组卷配置列表不存在"),
+    SUBJECT_INSERT_FAILED(230108,"试题插入失败"),
+    ANSWER_INSERT_FAILED(230109,"试题答案插入失败"),
+    CREATE_NEW_PAPER_FAILED(230110,"试卷创建失败"),
+    FETCH_SUBJECTS_FROM_BASE_DATA_SERVICE_FAILED(230111,"从基础数据服务抓取题目失败"),
+    SUBJECTS_NUMBERS_NOT_ENOUGH(230112,"题目数量不足"),
     ADD_PAPER_COMB_SYSTEM_ERROR(230103, "添加组卷信息的时候系统出错"),
     STANDARD_PAPER_CONFIGURE_ERROR(230104, "标准组卷的时候配置项信息有误"),
     STANDARD_PAPER_CONFIGURE_NON_EXISTENT(230105, "标准组卷的时候配置项不存在"),
@@ -17,6 +22,7 @@ public enum PaperExceptionEnum {
     UPLOAD_PAPER_NON_EXISTENT(230201, "试卷不存在"),
     UPLOAD_PAPER_ORGANIZATION_NON_EXISTENT(230202, "机构组织不存在"),
     UPLOAD_PAPER_REPEAT(230203, "试卷不可重复上传"),
+    UPLOAD_PAPER_FAILED(230204,"试卷上传失败"),
     /**
      * 试卷下载
      */
@@ -26,7 +32,12 @@ public enum PaperExceptionEnum {
      */
     MAINTAIN_PAPER_NON_EXISTENT(230401, "试卷不存在"),
     MAINTAIN_IN_USE(230402, "删除的试卷正在被使用"),
-    MAINTAIN_VERSION_DISACCORD(230403, "试卷版本不一致");
+    MAINTAIN_VERSION_DISACCORD(230403, "试卷版本不一致"),
+    ERROR_TEMPLATE_MARK(230404,"错误的模板标记"),
+    PAPER_INFO_UPDATE_FAILED(230405,"试卷信息更新失败"),
+    TOO_OLD_PAPER_VERSION(230406,"过旧的试卷版本"),
+    PAPER_DELETE_FAILED(230407,"试卷删除失败");
+
 
     /**
      * 异常码
