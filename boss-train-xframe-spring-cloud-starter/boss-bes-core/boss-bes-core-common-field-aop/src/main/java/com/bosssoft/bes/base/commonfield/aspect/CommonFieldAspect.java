df9ce252d4602d99183b9f9058ab28baeaee9c0f
wolfishness <961053359@qq.com>
2019-09-14 09:44:31 +0800

    修改自动填充公共字段中的时间类型转换失败

--- boss-train-xframe-spring-cloud-starter/boss-bes-core/boss-bes-core-common-field-aop/src/main/java/com/bosssoft/bes/base/commonfield/aspect/CommonFieldAspect.java
+++ boss-train-xframe-spring-cloud-starter/boss-bes-core/boss-bes-core-common-field-aop/src/main/java/com/bosssoft/bes/base/commonfield/aspect/CommonFieldAspect.java
@@ -22,17 +22,17 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.Objects;
 
 /**
  * 公共字段设置aspect
  *
  * @author 李旭阳
- * @createdTime 2019/8/15 11:08
  * @version 1.0.0
+ * @createdTime 2019/8/15 11:08
  * @updateBy 李旭阳
  * @updatedTime 2019/8/22 14:45
- *
  */
 @Aspect
 @Component
@@ -62,10 +62,12 @@ public class CommonFieldAspect {
      * 定义增强处理切入点为CommonField注解所注方法
      */
     @Pointcut("@annotation(com.bosssoft.bes.base.commonfield.annotation.SetCommonField)")
-    public void commonFieldPoint(){}
+    public void commonFieldPoint() {
+    }
 
     /**
-     *设置公共字段的方法
+     * 设置公共字段的方法
+     *
      * @param joinPoint 连接点
      */
     @Before("commonFieldPoint()")
@@ -83,26 +85,26 @@ public class CommonFieldAspect {
         String methodType = getMethodType(joinPoint);
 
         //根据方法类型从commonField对象中抽取不同字段设置进入切入点方法参数中
-        int result = setFields(commonField,joinPoint,methodType);
+        int result = setFields(commonField, joinPoint, methodType);
     }
 
     /**
      * 根据methodType抽取不同的commonField中字段设置进切入点方法参数
      *
      * @param commonField 取到的公共字段值
-     * @param joinPoint 连接点
-     * @param methodType 所切方法类型
+     * @param joinPoint   连接点
+     * @param methodType  所切方法类型
      * @return int 执行情况 0表示正常完成
      */
     private int setFields(CommonField commonField, JoinPoint joinPoint, String methodType) throws ServiceException {
-        if(!TYPE_INSERT.equals(methodType)){
+        if (!TYPE_INSERT.equals(methodType)) {
             commonField.setCreatedBy(null);
             commonField.setCreatedTime(null);
-            if(TYPE_QUERY.equals(methodType)){
+            if (TYPE_QUERY.equals(methodType)) {
                 commonField.setUpdatedBy(null);
                 commonField.setUpdatedTime(null);
             }
-            if(TYPE_UPDATE.equals(methodType)){
+            if (TYPE_UPDATE.equals(methodType)) {
                 commonField.setOrgId(null);
                 commonField.setCompanyId(null);
             }
@@ -110,10 +112,10 @@ public class CommonFieldAspect {
         //获取切点第一个参数
         Object[] args = joinPoint.getArgs();
         Object param = null;
-        if(null!=args){
+        if (null != args) {
             param = args[0];
         }
-        if(null==param){
+        if (null == param) {
             return 1;
         }
         Field[] fields = commonField.getClass().getDeclaredFields();
@@ -128,18 +130,24 @@ public class CommonFieldAspect {
                     //获取param对应的setter方法后执行
                     String fieldName = field.getName();
                     //by lujinshan 循环遍历参数中是否存在需要被填充的字段
-                    for (Field fieldParam : fieldParams){
+                    for (Field fieldParam : fieldParams) {
                         String fieldParamName = fieldParam.getName();
-                        if (fieldName.equals(fieldParamName)){
+                        if (fieldName.equals(fieldParamName)) {
                             fieldName = fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
-                            Method fieldSetter = param.getClass().getMethod("set" + fieldName, field.getType());
+                            Method fieldSetter = null;
+                            if (fieldParam.getType().getSimpleName().equals("Date")) {
+                                fieldSetter = param.getClass().getMethod("set" + fieldName, Date.class);
+                                fieldValue = new Date();
+                            } else {
+                                fieldSetter = param.getClass().getMethod("set" + fieldName, field.getType());
+                            }
                             fieldSetter.invoke(param, fieldValue);
                         }
                     }
 
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new ServiceException(SystemExceptionEnum.SYSTEM_BASE_COMMON_FIELD_SET_ERROR);
         }
         return 0;
@@ -148,13 +156,13 @@ public class CommonFieldAspect {
     /**
      * 获取请求中的用户id
      *
-     * @return 用户id,未找到返回null
+     * @return 用户id, 未找到返回null
      */
     private Long getUserIdFromRequest() throws ServiceException {
         HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder
                 .getRequestAttributes())).getRequest();
         String token = request.getHeader("token");
-        if(null == token){
+        if (null == token) {
             throw new ServiceException(SystemExceptionEnum.SYSTEM_BASE_COMMON_FIELD_REQUEST_PARSE_ERROR);
         }
 
@@ -162,7 +170,7 @@ public class CommonFieldAspect {
         String rsaRedisKey = "rsa_pub_key";
         ValueOperations ops = redisTemplate.opsForValue();
         RSAKey key = (RSAKey) ops.get(rsaRedisKey);
-        return (Long)JwtUtils.get(token,key,"userId");
+        return (Long) JwtUtils.get(token, key, "userId");
     }
 
     /**
@@ -171,7 +179,7 @@ public class CommonFieldAspect {
      * @param userId 用户id
      * @return 需要设置的公共字段对象
      */
-    private CommonField getCommonFieldByUserId(Long userId) throws ServiceException{
+    private CommonField getCommonFieldByUserId(Long userId) throws ServiceException {
         RedisTemplate redisTemplate = new RedisTemplate();
         String redisKey = "user_info_" + userId;
 
@@ -200,18 +208,18 @@ public class CommonFieldAspect {
      * @param joinPoint 连接点
      * @return 方法类型
      */
-    private String getMethodType(JoinPoint joinPoint){
-        Method method = ((MethodSignature)joinPoint.getSignature()).getMethod();
+    private String getMethodType(JoinPoint joinPoint) {
+        Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
         SetCommonField commonFieldAnno = method.getAnnotation(SetCommonField.class);
         String methodType = DEFAULT_METHOD_TYPE;
-        if(null!= commonFieldAnno) {
-            if(TYPE_UPDATE.equals(commonFieldAnno.methodType())){
+        if (null != commonFieldAnno) {
+            if (TYPE_UPDATE.equals(commonFieldAnno.methodType())) {
                 methodType = TYPE_UPDATE;
-            }else if(TYPE_QUERY.equals(commonFieldAnno.methodType())){
+            } else if (TYPE_QUERY.equals(commonFieldAnno.methodType())) {
                 methodType = TYPE_QUERY;
-            }else if(TYPE_INSERT.equals(commonFieldAnno.methodType())){
+            } else if (TYPE_INSERT.equals(commonFieldAnno.methodType())) {
                 methodType = TYPE_INSERT;
-            }else if(TYPE_DELETE.equals(commonFieldAnno.methodType())){
+            } else if (TYPE_DELETE.equals(commonFieldAnno.methodType())) {
                 methodType = TYPE_DELETE;
             }
         }
